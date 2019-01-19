4827a917df647cfa71e7c71250ef88a67be056ed
chpeih <peihuanchen01@gmail.com>
2019-01-19 14:43:55 +0800

    删除无用文件

--- MyExvimConfig/.exvim.proj/update-symbols.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export DEST="./.exvim.proj"
-export TOOLS="/home/chenpeihuan/exVim//vimfiles/tools/"
-export TMP="${DEST}/_symbols"
-export TARGET="${DEST}/symbols"
-sh ${TOOLS}/shell/bash/update-symbols.sh
--- MyExvimConfig/.exvim.proj/update-tags.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export DEST="./.exvim.proj"
-export TOOLS="/home/chenpeihuan/exVim//vimfiles/tools/"
-export CTAGS_CMD="ctags"
-export OPTIONS="--c++-kinds=+p --fields=+iaSl --extra=+q --python-kinds=-iv -R"
-export TMP="${DEST}/_tags"
-export TARGET="${DEST}/tags"
-sh ${TOOLS}/shell/bash/update-tags.sh
