29373f9b0d9bc4c64ec8e5007fd247a54fe868a9
plenty <peihuanchen01@gmail.com>
2019-01-19 12:23:10 +0800

    Update install.sh

--- bigo/install.sh
+++ bigo/install.sh
@@ -2,11 +2,11 @@
 
 cd ~
 echo "cp exVim dir start====================="
-cp -r MyExvimConfig/exVim .
+cp -r exvim/exVim .
 echo "cp exVim dir end====================="
 
 echo "cp local dir start====================="
-cp -r  MyExvimConfig/local .
+cp -r  exvim/local .
 echo "cp local dir end====================="
 
 echo "install exVim vundle start"
@@ -22,7 +22,7 @@ echo "chmod +x on local/bin end"
 cd -
 
 echo "overwrite .vimrc"
-mv MyExvimConfig/.vimrc .
+mv exvim/.vimrc .
 
 
 echo "\nexport PATH=~/local/bin:$PATH\n" >> .bashrc
